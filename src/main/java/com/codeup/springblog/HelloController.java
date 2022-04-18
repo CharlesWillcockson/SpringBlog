ce75f007e0c2d44c6a4a8b5a5b4eba56454d7ec9
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-18 15:21:04 -0500

    added Model to hello controller

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -18,5 +19,11 @@ public class HelloController {
 
 
         return "Hey " + name + ", I can't believe you are " + age + " already";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
