6fd52cf0fcea55abe3671050587785e753436896
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-18 14:10:47 -0500

    finished hello controller

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,8 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -11,5 +10,13 @@ public class HelloController {
     @ResponseBody
     public String hello(){
         return "Hello there";
+    }
+
+    @RequestMapping(path= "/hello/{name}/and/{age}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloNameAge(@PathVariable String name, @PathVariable int age){
+
+
+        return "Hey " + name + ", I can't believe you are " + age + " already";
     }
 }
