db919a545ad9f8085d2230d1c70fd8602326615c
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-18 16:40:17 -0500

    dice controller

---
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -0,0 +1,37 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String showGuessForm(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{userGuess}")
+    public String diceGuess(@PathVariable int userGuess, Model model) {
+        int random = (int) Math.ceil(Math.random() * 6);
+        String message;
+
+        if (random == userGuess) {
+            message = "You got it!";
+        } else {
+            message ="Nah Bruh!";
+        }
+
+        model.addAttribute("title", "Rolled results");
+        model.addAttribute("message", message);
+        model.addAttribute("userGuess", userGuess);
+        model.addAttribute("random", random);
+
+        return "rolledDice";
+    }
+
+}
