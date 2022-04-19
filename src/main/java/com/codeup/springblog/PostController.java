13977c03e3b44ad40a7461322a0a2b408588fca9
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-19 11:34:10 -0500

    updated post controller

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -2,21 +2,33 @@ package com.codeup.springblog;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String indexPage(){
-        return "This will be the index page of all posts";
+    public String posts(Model model){
+        ArrayList<Post> allPosts = new ArrayList<>();
+
+        allPosts.add(new Post("First post", "Post number 1 (Pee-pee)"));
+        allPosts.add(new Post("Second post", "Post number 2 (Doo-doo)"));
+
+        model.addAttribute("posts", allPosts);
+        return "posts/index";
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String individualPost(@PathVariable int id){
-        return "This is the future home of individual post number " + id;
+    public String individualPost(@PathVariable long id, Model model){
+
+        Post post = new Post(1,"Title of post", "Body of post");
+        model.addAttribute("post", post);
+
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
