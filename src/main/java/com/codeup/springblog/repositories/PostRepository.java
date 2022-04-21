913168f12d961ff1e0a6388d64ce4928c84ab1bf
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-21 14:55:03 -0500

    updated Post repo

--- src/main/java/com/codeup/springblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepository.java
@@ -2,6 +2,9 @@ package com.codeup.springblog.repositories;
 
 import com.codeup.springblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Post getPostById(long id);
 }
