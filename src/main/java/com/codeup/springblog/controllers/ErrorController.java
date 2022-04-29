09f154e3e81c2158645c502bbb29450a73ac2b78
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-29 16:55:59 -0500

    added error controller

---
+++ src/main/java/com/codeup/springblog/controllers/ErrorController.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(){
+        return "error";
+    }
+}
